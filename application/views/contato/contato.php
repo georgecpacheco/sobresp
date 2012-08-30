e0bdf04c34aba14b63759ba3950e5af27ca6282a
George Chaves Pacheco <georgecpacheco@gmail.com>
2012-08-30 18:40:05 -0300

    Criação formulário contato

--- application/views/contato/contato.php
+++ application/views/contato/contato.php
@@ -8,10 +8,19 @@
                 de Santa Maria, em região de fácil acesso com
                 estacionamento próprio, tendo a disponibilidade 
                 de um hotel conveniado em frente à sua sede:</p>
+             <h4>Rua Appel, 520 | CEP: 97015-030 | Santa Maria - RS</h4>
+             <h4>(55) 3214-1111</h4>
+             <h4>(55) 3219-0482</h4>
         </div>
         <div id="end_2">
            <?php echo img('libraries/img/mapa.jpg'); ?>
         </div>
+        <div id="form_contato">
+            <h2>Fale Conosco</h2>
+            <p>Preencha o formulário para entrar em contato conosco.</p>
+            formulario
+        </div>
+       
     </div>
     <div id="col_dir">
         <h1>Notícias</h1>
